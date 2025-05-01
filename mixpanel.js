71b512b90e91c4b998a178dde4502457c347ee5a
Burak Ongoren <burakongoren@std.iyte.edu.tr>
2025-05-02 01:29:06 +0300

    mixpanel-remove

--- mixpanel.js
+++
@@ -1,9 +0,0 @@
-//Import Mixpanel SDK
-import mixpanel from "mixpanel-browser";
- 
-// Near entry of your product, init Mixpanel
-mixpanel.init("829a6ea5b9006b1c7635d9b8a201b23b", {
-  debug: true,
-  track_pageview: true,
-  persistence: "localStorage",
-});
